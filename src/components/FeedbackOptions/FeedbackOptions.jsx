ad34faf2c1e990a8e144155ce426e9f6972f8acb
FelixPolkanov <felixpolkanov@gmail.com>
2023-01-08 15:15:24 +0200

    update task

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -3,13 +3,15 @@ import { ButtonsWrapper, Button } from './FeedbackOptions.styled';
 
 
 export function FeedbackOptions({ options, onLeaveFeedback }) {
-  <ButtonsWrapper>
+  return (
+    <ButtonsWrapper>
     {options.map(option => (
       <Button key={option} onClick={() => onLeaveFeedback(option)}>
         {option}
       </Button>
     ))}
-  </ButtonsWrapper>
+    </ButtonsWrapper>
+  )
 }
 
   
